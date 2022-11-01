007b430d2f0474294a1cb861471ccda62f560654
David <david@syloper.com>
2022-10-31 21:59:29 -0300

    Fixed issue including correct chars

--- js/script.js
+++ js/script.js
@@ -1,81 +1,112 @@
 const elements = Object.seal({
-  input: document.getElementById("secure-password"),
-  copyButton: document.getElementById("copy"),
-  regenerateButton: document.getElementById("regenerate"),
-  copiedText: document.getElementById("copied-text"),
-  charactersLengthText: document.getElementById("characters-length"),
+    input: document.getElementById("secure-password"),
+    copiedText: document.getElementById("copied-text"),
+    rangeSlider: document.getElementById("length"),
+    charactersLengthText: document.getElementById("characters-length"),
 });
 
 const config = Object.seal({
-  characters: "abcdefghijklmnopqrstuvwxyz",
-  numbers: "0123456789",
-  symbols: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+    characters: "abcdefghijklmnopqrstuvwxyz",
+    numbers: "0123456789",
+    symbols: "!@#$%^&*()_+~`|}{[]:;?><,./-=",
 });
 
 const flags = Object.create({
-  length: 16,
-  lowercase: true,
-  uppercase: true,
-  numbers: true,
-  symbols: true,
+    length: 16,
+    lowercase: true,
+    uppercase: true,
+    numbers: true,
+    symbols: true,
 });
 
 let timeOut = null;
 
-function generatePassword() {
-  const characterList = [
-    ...(flags.lowercase ? config.characters : []),
-    ...(flags.uppercase ? config.characters.toUpperCase() : []),
-    ...(flags.numbers ? config.numbers : []),
-    ...(flags.symbols ? config.symbols : []),
-  ].join("");
+function randomNumber(limit) {
+    return Math.floor(Math.random() * limit);
+}
 
-  return Array.from({ length: flags.length }, () =>
-    Math.floor(Math.random() * characterList.length)
-  )
-    .map((number) => characterList[number])
-    .join("");
+function generatePassword() {
+    const characterList = [
+        ...(flags.lowercase ? config.characters : []),
+        ...(flags.uppercase ? config.characters.toLocaleUpperCase() : []),
+        ...(flags.numbers ? config.numbers : []),
+        ...(flags.symbols ? config.symbols : []),
+    ].join("");
+
+    // At least one of each with flag on true
+    const required = [
+        flags.lowercase
+            ? config.characters[randomNumber(config.characters.length)]
+            : "",
+        flags.uppercase
+            ? config.characters[
+                  randomNumber(config.characters.length)
+              ].toLocaleUpperCase()
+            : "",
+        flags.numbers
+            ? config.numbers[randomNumber(config.numbers.length)]
+            : "",
+        flags.symbols
+            ? config.symbols[randomNumber(config.symbols.length)]
+            : "",
+    ].join("");
+
+    const string = Array.from({ length: flags.length - required.length }, () =>
+        randomNumber(characterList.length)
+    )
+        .map((number) => characterList[number])
+        .join("");
+
+    return string + required;
 }
 
 function updateInput() {
-  elements.input.value = generatePassword();
+    elements.input.value = generatePassword();
 }
 
 function showCopiedText() {
-  elements.copiedText.classList.add("active");
-  clearTimeout(timeOut);
-  timeOut = setTimeout(() => {
-    elements.copiedText.classList.remove("active");
-  }, 3000);
+    elements.copiedText.classList.add("active");
+    clearTimeout(timeOut);
+    timeOut = setTimeout(() => {
+        elements.copiedText.classList.remove("active");
+    }, 3000);
 }
 
-document.getElementById("length").addEventListener("input", (event) => {
-  flags.length = elements.charactersLengthText.textContent = event.target.value;
-  updateInput();
+// Event delegation
+document.body.addEventListener("click", (event) => {
+    switch (event.target.dataset.ref) {
+        case "switch":
+            flags[event.target.id] = event.target.checked;
+            updateInput();
+            break;
+
+        case "copy":
+            if (!navigator.clipboard) {
+                elements.input.select();
+                document.execCommand("copy");
+                elements.input.blur();
+                showCopiedText();
+            } else {
+                navigator.clipboard
+                    .writeText(elements.input.value)
+                    .then(showCopiedText)
+                    .catch((e) => console.log(e));
+            }
+            break;
+
+        case "regenerate":
+            updateInput();
+            break;
+
+        default:
+            break;
+    }
 });
 
-Array.from(document.getElementsByClassName("listen")).forEach((element) => {
-  element.addEventListener("change", (event) => {
-    flags[event.target.id] = event.target.checked;
+elements.rangeSlider.addEventListener("input", (event) => {
+    flags.length = elements.charactersLengthText.textContent =
+        event.target.value;
     updateInput();
-  });
 });
 
-elements.copyButton.addEventListener("click", () => {
-  if (!navigator.clipboard) {
-    elements.input.select();
-    document.execCommand("copy");
-    elements.input.blur();
-    showCopiedText();
-  } else {
-    navigator.clipboard
-      .writeText(elements.input.value)
-      .then(showCopiedText)
-      .catch((e) => console.log(e));
-  }
-});
-
-elements.regenerateButton.addEventListener("click", updateInput);
-window.addEventListener("load", () => {
-  updateInput();
-});
+updateInput();
